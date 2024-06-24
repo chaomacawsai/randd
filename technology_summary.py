caa04ed53a2a3a2c7dce6e532bbd7b08d6816b57
chaomacawsai <chao.liu@macaws.ai>
2024-06-24 23:03:50 +0100

    Create technology_summary.py

---
+++ technology_summary.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+from decouple import config  # Import config from decouple
+openai = OpenAI(default_headers={"OpenAI-Beta": "assistants=v1"})
+
+def summary(full_response):
+    #print("**************")
+    full_response = full_response + "Write a paragraph in 200 words summarising the technology.Do not use bullet points"
+    #print(full_response)
+    response = openai.chat.completions.create(
+        model="gpt-4o", 
+        messages=[
+            {   "role": "system",
+            "content" : "You are a technology reviewer. Your audience is technology enthusiast. ",
+                "role":"user",
+            "content": full_response
+            
+            }
+        ]
+    )
+    
+    summary_text = response.choices[0].message.content
+    print("*************")
+    print(summary_text)
+    return summary_text
